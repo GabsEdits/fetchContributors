6b81c5a875b0a932662e357017a118818396564c
Gabriel Cozma <me@gabs.eu.org>
2023-12-24 15:26:48 +0200

    Simplified Script

--- fetchContributors.js
+++ fetchContributors.js
@@ -1,5 +1,4 @@
-import fetch from 'node-fetch';
-import fs from 'fs/promises';
+import { writeFile } from 'fs/promises';
 
 async function fetchContributorsData() {
   let contributors = [];
@@ -9,15 +8,18 @@ async function fetchContributorsData() {
   };
 
   try {
+    console.log('Fetching organization repositories...');
     const orgReposResponse = await fetch('https://api.github.com/orgs/Vanilla-OS/repos', { headers });
     const orgReposData = await orgReposResponse.json();
 
     for (const repo of orgReposData) {
+      console.log(`Getting data for repository ${repo.name}...`);
       const contributorsResponse = await fetch(repo.contributors_url, { headers });
       const contributorsData = await contributorsResponse.json();
 
       for (const contributor of contributorsData) {
         if (!contributors.some((c) => c.login === contributor.login)) {
+          console.log(`Getting data for the contributor ${contributor.login}...`);
           const userDetailsResponse = await fetch(`https://api.github.com/users/${contributor.login}`, { headers });
           const userDetails = await userDetailsResponse.json();
 
@@ -32,7 +34,9 @@ async function fetchContributorsData() {
       }
     }
 
-    await fs.writeFile('contributors.json', JSON.stringify(contributors, null, 2));
+    console.log('Writing contributors data to file...');
+    await writeFile('contributors.json', JSON.stringify(contributors, null, 2));
+    console.log('Script completed successfully! Check contributors.json file.');
   } catch (error) {
     console.error('Error fetching contributors:', error);
   }
